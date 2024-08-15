0ca28c120b31e6ea9ed46423be8fe23a38579aee
OlgaKaratkevich <volhakaratkevich@gmail.com>
2024-08-15 23:43:51 +0300

    -

--- com/lesson3/Main.java
+++ com/lesson3/Main.java
@@ -1,17 +1,19 @@
 package com.lesson3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Введите количество элементов массива");
-//        Scanner scan = new Scanner(System.in);
-//        int n = scan.nextInt();
-//        Fibonacci fibonacci = new Fibonacci();
-//        fibonacci.generateFibonacciArray(n);
+        System.out.println("Введите количество элементов массива");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.generateFibonacciArray(n);
 
-//        MinPlusMax minPlusMax = new MinPlusMax();
-//        minPlusMax.minMax(args);
+        MinPlusMax minPlusMax = new MinPlusMax();
+        minPlusMax.minMax(args);
 
-//        EvenSumAndOddSumDiff evenSumAndOddSumDiff = new EvenSumAndOddSumDiff();
-//        evenSumAndOddSumDiff.findDiff(args);
+        EvenSumAndOddSumDiff evenSumAndOddSumDiff = new EvenSumAndOddSumDiff();
+        evenSumAndOddSumDiff.findDiff(args);
     }
 }
